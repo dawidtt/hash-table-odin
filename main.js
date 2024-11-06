4703cb308f8ca9734a76c21a5b3837869ad2a8e2
dawidtt <dawid.gorszka@gmail.com>
2024-11-06 20:49:28 +0100

    To do hashing

--- main.js
+++ main.js
@@ -3,12 +3,15 @@ class HashMap {
     this.buckets = new Array(16).fill(null);
   }
   growHash() {
-    const capacity = 0.8;
+    const capacity = 0.75;
     let counter = 0;
     this.buckets.forEach((bucket) => {
       if (bucket !== null) counter++;
     });
-    if (counter > this.buckets.length * capacity) this.buckets.push(null);
+    console.log(counter);
+    console.log(Math.floor(this.buckets.length * capacity));
+    if (counter > Math.floor(this.buckets.length * capacity))
+      this.buckets.push(null);
   }
   hash(key) {
     this.growHash();
@@ -17,9 +20,8 @@ class HashMap {
     const primeNumber = 31;
     for (let i = 0; i < key.length; i++) {
       hashCode = primeNumber * hashCode + key.charCodeAt(i);
-      hashCode = hashCode % this.buckets.length;
     }
-
+    hashCode = hashCode % this.buckets.length;
     return hashCode;
   }
   set(key, value) {
@@ -94,13 +96,21 @@ class HashMap {
   }
 }
 
-let hashMap = new HashMap();
-hashMap.set("Carlos", "carlo");
-console.log(hashMap);
-console.log(hashMap.keys());
-console.log(hashMap.values());
-console.log(hashMap.entries());
+const test = new HashMap();
+test.set("apple", "red");
+test.set("banana", "yellow");
+test.set("carrot", "orange");
+test.set("dog", "brown");
+test.set("elephant", "gray");
+test.set("frog", "green");
+test.set("grape", "purple");
+test.set("hat", "black");
+test.set("ice cream", "white");
+test.set("jacket", "blue");
+test.set("kite", "pink");
+test.set("lion", "golden");
+console.log(test);
 
-hashMap.clear();
-console.log(hashMap);
-console.log(hashMap.has("Carlos"));
+test.set("moon", "silver");
+
+console.log(test);
